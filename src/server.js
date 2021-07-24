71289dd6d45c9ef8315bbdc54b00a9927bbf0abc
eunsu29 <edit1643@gmail.com>
2021-07-24 12:51:58 +0900

    Recap Setup, Servers, Controllers and Middleware 210724

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("<h1>Home</h1>");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login Here");
-};
-
+app.use(logger);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handelListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
