b45bfb06062829954ea2ca226ec3908186829e74
eunsu29 <edit1643@gmail.com>
2021-07-24 13:43:38 +0900

    Separate Routers and Controllers 210724

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("<h1>Home</h1>");
-};
-
 app.use(logger);
-app.get("/", handleHome);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handelListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
