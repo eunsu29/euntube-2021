a473d5493884ba6009f4e100d607248dba964032
eunsu29 <edit1643@gmail.com>
2021-07-26 14:34:33 +0900

    Add a Upload Function, Installing and Connecting MongoDB 210725

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -18,6 +19,6 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handelListening = () =>
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`✅ Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handelListening);
