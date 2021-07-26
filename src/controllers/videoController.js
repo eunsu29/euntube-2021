668a4a2d6eec5cbbcf5a8e92e590418c029fe339
eunsu29 <edit1643@gmail.com>
2021-07-26 23:43:54 +0900

    Using mongoose and Making Video Model 210726

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,20 @@
-let videos = [
-  {
-    title: "1st Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "2nd Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 0,
-    id: 2,
-  },
-  {
-    title: "3rd Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: video.title, video });
+  return res.render("watch");
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: "Editing" });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  const video = videos[id - 1];
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -51,14 +23,5 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
